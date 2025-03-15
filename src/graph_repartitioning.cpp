ef44351d422ae70ae900055e40e3d35e9b538918
Jacob Rose <il275@stud.uni-heidelberg.de>
2025-03-15 22:22:54 +0100

    Add readConfigFile for sharedMap

--- src/graph_repartitioning.cpp
+++ src/graph_repartitioning.cpp
@@ -7,6 +7,9 @@
 #include "../include/graph_io.hh"
 
 #include <libsharedmap.h>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;    
 
 //! Wichtig hier: Node format umwandeln von 1...n
 //! zu 0...n-1
@@ -45,6 +48,73 @@ std::vector<long> readNumberNodesAndEdgesFromFile(std::ifstream& file) {
     return numberNodesAndEdges;
 }
 
+bool readConfigFileSharedMap(const std::string& filename, std::vector<int>& hierarchy, std::vector<int>& distance, int& l, float& imbalance, int& n_threads, int& seed, shared_map_strategy_type_t& strategy, shared_map_algorithm_type_t& parallel_alg, shared_map_algorithm_type_t& serial_alg, bool& verbose_error, bool& verbose_statistics) {
+    std::ifstream file(filename); // Datei im Textmodus öffnen
+    if (!file) {
+        std::cerr << "Fehler beim Öffnen der Datei: " << filename << std::endl;
+        return false;
+    }
+
+    json j;
+    file >> j;
+
+    hierarchy = j["hierarchy"].get<std::vector<int>>();
+    distance = j["distance"].get<std::vector<int>>();
+    l = j["l"].get<int>();
+    imbalance = j["imbalance"].get<float>();
+    n_threads = j["n_threads"].get<int>();
+    seed = j["seed"].get<int>();        
+    verbose_error = j["verbose_error"].get<bool>();
+    verbose_statistics = j["verbose_statistics"].get<bool>();
+
+
+    // die muss ich anders behandeln:
+    // zuerst als String extrahieren und dann zuordnen
+    std::string strategy_string = j["strategy"].get<std::string>();
+    if(strategy_string == "NAIVE") {
+        strategy = NAIVE;
+    } else if(strategy_string == "LAYER") {
+        strategy = LAYER;
+    } else if(strategy_string == "QUEUE") {
+        strategy = QUEUE;
+    } else if(strategy_string == "NB_LAYER") {
+        strategy = NB_LAYER;
+    } else {
+        std::cerr << "Strategy not found!" << std::endl;
+        return false;
+    }
+
+
+    std::string parallel_alg_string = j["parallel_alg"].get<std::string>();
+    if(parallel_alg_string == "MTKAHYPAR_DEFAULT") {
+        parallel_alg = MTKAHYPAR_DEFAULT;
+    } else if(parallel_alg_string == "MTKAHYPAR_QUALITY") {
+        parallel_alg = MTKAHYPAR_QUALITY;
+    } else if(parallel_alg_string == "MTKAHYPAR_HIGHEST_QUALITY") {
+        parallel_alg = MTKAHYPAR_HIGHEST_QUALITY;
+    } else {
+        std::cerr << "Parallel Algorithm not found!" << std::endl;
+        return false;
+    }
+
+    
+    std::string serial_alg_string = j["serial_alg"].get<std::string>();
+    if(serial_alg_string == "KAFFPA_ECO") {
+        serial_alg = KAFFPA_ECO;
+    } else if(serial_alg_string == "KAFFPA_FAST") {
+        serial_alg = KAFFPA_FAST;
+    } else if(serial_alg_string == "KAFFPA_STRONG") {
+        serial_alg = KAFFPA_STRONG;
+    } else {
+        std::cerr << "Serial Algorithm not found!" << std::endl;
+        return false;
+    }
+    
+
+    return true;
+}
+
+
 int main(int argc, char* argv[]) {
 
     /*
@@ -74,7 +144,7 @@ int main(int argc, char* argv[]) {
     Graph g(numberNodesAndUpdates[0]);
 
     int linesRead = 0;
-    for(int i = 0; i < 3330022 ; i++) {
+    for(int i = 0; i <  50 /*3330022 */ ; i++) {
         edge = readEdgeInformationFromFile(file);
         
         if(edge[0] == 1) {
@@ -106,26 +176,48 @@ int main(int argc, char* argv[]) {
   //TODO: Das Partitioningtool von Henning einbinden
   //Steps:
     //1. Graph in CSR Format umwandeln
-    //2. Die anderen Paramter setzen (vielleicht kann ich die durch file einlesen)
-
-
-    int hierarchy[] = {2, 2};
-    int distance[]  = {1, 10};
-    int l           = 2;
-
-    // imbalance 3%, 1 thread, seed 0
-    float imbalance = 0.03;
-    int   n_threads = 1;
-    int   seed      = 0;
-
-    shared_map_strategy_type_t  strategy     = NB_LAYER;                    // distribution strategy Non-Blocking Layer
-    shared_map_algorithm_type_t parallel_alg = MTKAHYPAR_HIGHEST_QUALITY;   // parallel algorithm Mt-KaHyPar Highest Quality
-    shared_map_algorithm_type_t serial_alg   = KAFFPA_STRONG;               // serial algorithm KaFFPa Strong
-
-    // whether to print statistics
-    bool verbose_error      = true;
-    bool verbose_statistics = true;
+    
+    
+    //2. Die anderen Paramter setzen 
+    //! convert to int* later
+    std::vector<int> hierarchy;
+    std::vector<int> distance;
+    int l;
+    float imbalance;
+    int   n_threads;
+    int   seed;
+    shared_map_strategy_type_t  strategy;                    
+    shared_map_algorithm_type_t parallel_alg ;
+    shared_map_algorithm_type_t serial_alg;
+    bool verbose_error;
+    bool verbose_statistics;
    
+    bool configSucces = readConfigFileSharedMap("./res/sharedMap_config.json", hierarchy, distance, l, imbalance, n_threads, seed, strategy, parallel_alg, serial_alg, verbose_error, verbose_statistics);
+
+    // optional. just nice debugging rn
+    if(configSucces) {std::cout << "Hierarchy contents: ";
+        for (const auto& level : hierarchy) {
+            std::cout << level << " ";
+        }
+        std::cout << std::endl;
+        std::cout << "Distance contents: ";
+        for (const auto& dist : distance) {
+            std::cout << dist << " ";
+        }
+        std::cout << std::endl;
+
+        std::cout << "L: " << l << std::endl;
+        std::cout << "Imbalance: " << imbalance << std::endl;
+        std::cout << "Number of Threads: " << n_threads << std::endl;
+        std::cout << "Seed: " << seed << std::endl;
+        std::cout << "Strategy: " << strategy << std::endl;
+        std::cout << "Parallel Algorithm: " << parallel_alg << std::endl;
+        std::cout << "Serial Algorithm: " << serial_alg << std::endl;
+        std::cout << "Verbose Error: " << std::boolalpha << verbose_error << std::endl;
+        std::cout << "Verbose Statistics: " << std::boolalpha << verbose_statistics << std::endl;
+    }
+
+
 
 
     return 0;

506a751dca3f7f993448542a9279a0f15aa8ac2a
Jacob Rose <il275@stud.uni-heidelberg.de>
2025-04-11 17:39:49 +0200

    add createSubMatrices

--- include/graphRFSMultilevel.hh
+++ include/graphRFSMultilevel.hh
@@ -3,12 +3,20 @@
 
 #include "graph.hh"
 #include <vector>
+#include <tuple>
 
 class graphRFSMultilevel : public Graph {
     private:
+    
+    std::vector<std::vector<int>> createSimilarityMatrix(const std::vector<long>& old_partition, const std::vector<long>& new_partition, int k);
+    std::vector<std::tuple<int, int>> heuristicAssignment(const std::vector<std::vector<int>>& simMatrix);
 
     std::vector<std::vector<int>> shrinkMatrix(std::vector<std::vector<int>> simMatrix, int hierarchy  );
-    std::vector< std::vector<std::vector<int>>  > createAllSimMatrices(std::vector<std::vector<int>> simMatrix, std::vector<int> hierarchyArray);
+    std::vector< std::vector<std::vector<int>> > createAllSimMatrices(std::vector<std::vector<int>> simMatrix, std::vector<int> hierarchyArray);
+    
+    std::vector< std::vector<std::vector<int>> > createSubMatrices(std::vector<std::vector<int>> simMatrix, std::vector<std::tuple<int, int>> matching, int hierarchy);
+    // func match
+    // return type is matching  shiftIndexMatching();
 
 
     public:

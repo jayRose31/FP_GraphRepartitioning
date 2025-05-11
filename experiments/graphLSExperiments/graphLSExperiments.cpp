e9dbdc4aa18748f1666df42f491ff9297419e599
Jacob Rose <il275@stud.uni-heidelberg.de>
2025-05-11 17:16:55 +0200

    add first experiment (TODO: recheck i didnt make any mistakes)

---
+++ experiments/graphLSExperiments/graphLSExperiments.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <string> 
+#include <chrono>
+
+#include "../../include/graph.hh"
+#include "../../include/graphLocalSearch.hh"
+#include "../../include/graph_io.hh"
+#include "../../include/fileUtils.hh"
+
+int main(int argc, char* argv[]) {
+    
+
+    //------------------------------- init ------------------------------------------
+
+    // Check if the correct number of arguments is provided
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <configFile> <filename> <number_of_updates>" << std::endl;
+        return 1;
+    }
+
+    // Read arguments from the command line
+    std::string configFile = argv[1];  
+    std::string graphFilename = argv[2];   
+    int numberOfUpdates = std::stoi(argv[3]);
+    
+
+    // Assert that the third argument is a valid integer
+    assert((numberOfUpdates = std::stoi(argv[3])) >= 0 && "Number of updates must be a non-negative integer");
+
+
+    std::ifstream file(graphFilename); 
+    if (!file) {
+        std::cerr << "Fehler beim Öffnen der Datei: " << graphFilename << std::endl;
+        return 1;
+    }
+
+    std::cout << "starte experiment" << std::endl;
+
+    std::vector<long> edge;
+
+    std::vector<long> numberNodesAndUpdates = fileUtils::readNumberNodesAndEdgesFromFile(file);
+
+
+
+    // ------------------------------- build graph and partition --------------------------------
+    graphLocalSearch g(numberNodesAndUpdates[0]);
+
+
+    // Kanten einlesen
+    for(int i = 0; i < numberNodesAndUpdates[1] *2/ 3 ; i++) {
+        edge = fileUtils::readEdgeInformationFromFile(file);
+        
+        if(edge[0] == 1) {
+            g.add_edge(edge[1], edge[2]);
+            
+        } else if(edge[0] == 0) {
+            g.remove_edge(edge[1], edge[2]);
+            
+        }
+    }
+
+    
+    g.determine_initial_partition(configFile);
+    g.set_imbalance_from_file(configFile);
+    g.set_number_partitions_from_file(configFile);
+    
+    for(int i = 0; i < numberOfUpdates ; i++) {
+        edge = fileUtils::readEdgeInformationFromFile(file);
+        
+        if(edge[0] == 1) {
+            g.add_edge(edge[1], edge[2]);
+            
+        } else if(edge[0] == 0) {
+            g.remove_edge(edge[1], edge[2]);
+            
+        }
+    }
+
+    // Zeit messen
+    auto start = std::chrono::high_resolution_clock::now(); // Uhr starten
+
+    g.repartition(configFile);
+    
+    auto stop = std::chrono::high_resolution_clock::now(); // Uhr stoppen
+    
+    // Dauer berechnen
+    //auto duration_repartitioning = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+    std::chrono::duration<double> duration_repartitioning = stop - start;
+
+    // fertig
+    file.close();
+
+    
+
+    // ---------- Schreibe Sachen damit ich in python das Tool von Henning benutzen kann ------------
+    std::string filename = "./experiments/graphLSExperiments/analyzerTool_temporary.txt";
+    std::ofstream analyzer_tool(filename); 
+    if (!analyzer_tool) {
+        std::cerr << "Fehler beim Öffnen der Datei: " << filename << std::endl;
+        return 1;
+    }
+
+
+    std::string graph_metis_filename = "/home/jacob/Dokumente/AldaPraktikum/Code/experiments/graphLSExperiments/Graph_metis" ;
+    std::string graph_partition_filename = "/home/jacob/Dokumente/AldaPraktikum/Code/experiments/graphLSExperiments/Graph_partition";
+    std::string out_graph_filename = "/home/jacob/Dokumente/AldaPraktikum/Code/experiments/graphLSExperiments/out_graph" ;
+    
+    GraphIo io;
+    io.writeGraphToFileMetis(graph_metis_filename, g);
+    io.writePartitionToFile(graph_partition_filename, g);
+
+    analyzer_tool <<  graph_metis_filename << std::endl;
+    analyzer_tool <<  graph_partition_filename << std::endl;
+    analyzer_tool <<  out_graph_filename << std::endl;
+    
+
+    // Die anderen Paramter holen 
+    // Alles egal außer: hierarchy, distance und epsilon
+    std::vector<int> hierarchy;
+    std::vector<int> distance;
+    int l;
+    float imbalance;
+    int   n_threads;
+    int   seed;
+    shared_map_strategy_type_t  strategy;                    
+    shared_map_algorithm_type_t parallel_alg ;
+    shared_map_algorithm_type_t serial_alg;
+    bool verbose_error;
+    bool verbose_statistics;
+
+    bool configSucces = fileUtils::readConfigFileSharedMap(configFile, hierarchy, distance, l, imbalance, n_threads, seed, strategy, parallel_alg, serial_alg, verbose_error, verbose_statistics);
+
+    if(!configSucces) {
+        std::cerr << "Error reading config file. Exiting." << std::endl;
+        return 1;
+    }
+
+
+    for (size_t i = 0; i < hierarchy.size(); ++i) {
+        analyzer_tool << hierarchy[i];
+        if (i < hierarchy.size() - 1) {
+            analyzer_tool << ":";
+        }
+    }
+    analyzer_tool << std::endl;
+
+
+    for (size_t i = 0; i < distance.size(); ++i) {
+        analyzer_tool << distance[i];
+        if (i < distance.size() - 1) {
+            analyzer_tool << ":";
+        }
+    }
+    analyzer_tool << std::endl; 
+
+    analyzer_tool << imbalance << std::endl;
+
+    analyzer_tool.close();
+
+    // ---------------------------------------- pass remaining data to python script--------------------------------------------
+
+
+
+    // Datei öffnen zum schreiben der Ergebnisse
+    filename = "./experiments/graphLSExperiments/results_temporary.txt";
+    std::ofstream res_temp(filename); 
+    if (!res_temp) {
+        std::cerr << "Fehler beim Öffnen der Datei: " << filename << std::endl;
+        return 1;
+    }
+
+    // Ergebnisse schreiben
+    // 1. Zeit zum repartitionieren in sekunden
+    double duration_in_seconds = duration_repartitioning.count() /*  / 1000.0  */;
+    res_temp << duration_in_seconds << std::endl;
+
+    //2. migration cost: what percent of nodes changed partition
+    res_temp << g.get_migrationCost() << std::endl;
+
+    
+
+    
+    res_temp.close();
+
+
+    //--------------------------------- ----------------- done ------------------------------------------------------
+    
+    return 0;
+}
